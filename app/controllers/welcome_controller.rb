2a934bee3cb02f18615a987fe9acdc2c64f2bb63
Clive Yip <yipsifu@gmail.com>
2016-11-26 21:21:38 +0000

    change logic to reflect new api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,7 +22,8 @@ class WelcomeController < ApplicationController
     route_id = '258-MI'
 
     #  First, get all the trips
-    url = 'https://getgo-api.herokuapp.com/agencies/GO/routes/' + route_id + '/trips?date=' + date
+    url = 'https://getgo-api.herokuapp.com/routes/' + route_id + '/trips?date=' + date
+    # https://getgo-api.herokuapp.com/routes/258-MI/trips?date=20161202
     response = HTTParty.get(url)
     tripsHash = JSON.parse(response.body)
     tripsArray = tripsHash['trips']
@@ -32,27 +33,41 @@ class WelcomeController < ApplicationController
       puts trip['id']
     end
 
-    # https://getgo-api.herokuapp.com/agencies/GO/routes/258-MI/trips/6239-Fri-167/stop_times
 
     # Second, from the first trip, get the stops
     # ASSUMPTION: any given trip for the same route (or route variant) always returns the same stops
     # so we can just pick any trip (the first trip) from the route to obtain the stops
     trip_id_first = tripsArray[0]['id']
-    url = 'https://getgo-api.herokuapp.com/agencies/GO/routes/' + route_id + '/trips/' + trip_id_first + '/stop_times'
+    # https://getgo-api.herokuapp.com/trips/6239-Fri-167/stops
+    url = 'https://getgo-api.herokuapp.com/' + '/trips/' + trip_id_first + '/stops'
     response = HTTParty.get(url)
-    stopTimesHash = JSON.parse(response.body)
-    stopTimesArray = stopTimesHash['stop_times']
+    stopsHash = JSON.parse(response.body)
+    stopsArray = stopsHash['stops']
 
-    puts "---------- Task 2b: Stops stop_id ----------"
-    stopTimesArray.each do |stopTime|
-      puts stopTime['stop_id']
+    puts "---------- Task 2b: Stops names ----------"
+    stopsArray.each do |stop|
+      puts stop['name']
     end
 
-    # TODO: Third, get the stop_names from stop_id
 
     # TODO: do route variant for buses (current solution only for trains)
 
-    # Task 3. Get stop_times from stops and date and time
+    # Task 3. Get stop_times from  toStop, fromStop, date, and time
+    toStop = "UN"
+    fromStop = "SR"
+
+    # First, determine the direction based on toStop and fromStop
+    puts "---------- Task 3a: direction ----------"
+
+    # Second, get all the trips for that direction (odd/even number in tripsArray)
+    # ASSUMPTION: all the trips have alternating head-signs
+
+
+    # Third, for each trip, get the departure_time for the desired stop (by referecning stop_sequence)
+
+
+    # TODO: take care of bus routes that replace trains in non-rush hours (e.g. Bus 21 for Milton Train)
+    # TODO: test other train route
 
     # byebug;
   end
