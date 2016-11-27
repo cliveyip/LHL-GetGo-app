f983ab71310711255f9b18419c698a6d136509d0
Clive Yip <yipsifu@gmail.com>
2016-11-27 23:10:56 +0000

    refractor into smaller functions and added route variant

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,28 @@
 class WelcomeController < ApplicationController
+  # Run this on http://localhost:3000/welcome/index
   def index
-
-    # Run this on http://localhost:3000/welcome/index
-
     # ----- Task 1. Get all route names -----
+    # puts getRoutes
+
+    # ----- Task 2. Get stops from route and date -----
+    date = '20161202'
+    route_id = '258-21'
+    route_variant = '21D'
+    puts getStops(getTrips(date, route_id, route_variant)[0])
+
+    # ----- Task 3. Get stop_times from toStop, fromStop, date, and time -----
+    toStop = "01436"    # "Streetsville GO Station Parking Lot"
+    fromStop = "USBT"   # "Union Station Bus Terminal"
+    date = '20161202'
+    route_id = '258-21'
+    route_variant = '21D'
+
+    getArrivalTimes(date, route_id, route_variant, toStop, fromStop)
+  end
+
+  # helper functions
+  def getRoutes
+    routeNames = []
     url = 'https://getgo-api.herokuapp.com/agencies/GO/routes/'
     response = HTTParty.get(url)
     routesHash = JSON.parse(response.body)
@@ -11,16 +30,12 @@ class WelcomeController < ApplicationController
 
     puts "---------- Task 1: Route names ----------"
     routesArray.each do |route|
-      puts route['long_name']
+      routeNames << route['long_name']
     end
+    return routeNames
+  end
 
-    # ----- Task 2. Get stops from route and date -----
-
-    # User supplies the route and date
-    # TODO: get route_id from long_name
-    date = '20161202'
-    route_id = '258-MI'
-
+  def getTrips (date, route_id, route_variant)
     #  First, get all the trips
     url = 'https://getgo-api.herokuapp.com/routes/' + route_id + '/trips?date=' + date
     # https://getgo-api.herokuapp.com/routes/258-MI/trips?date=20161202
@@ -28,43 +43,41 @@ class WelcomeController < ApplicationController
     tripsHash = JSON.parse(response.body)
     tripsArray = tripsHash['trips']
 
-    puts "---------- Task 2a: Trip names ----------"
-    tripsArray.each do |trip|
-      puts trip['id']
+    tripsWithCorrectVariant = tripsArray.find_all {
+      |trip| trip['route_variant'] == route_variant
+    }
+
+    puts "---------- Task 2a: Trips under the specified route variant ----------"
+    tripsWithCorrectVariant.each do |trip|
+      print "Trip_id: ", trip['id'], ",  Route Variant: ", + trip['route_variant'] + ",  Direction: " + trip['direction_id'] + "\n"
     end
 
+    return tripsWithCorrectVariant
+  end
+
+  def getStops (trip)
     # Second, from the first trip, get the stops
-    # ASSUMPTION: any given trip for the same route (or route variant) always returns the same stops
-    # so we can just pick any trip (the first trip) from the route to obtain the stops
-    trip_id_first = tripsArray[0]['id']
-    # https://getgo-api.herokuapp.com/trips/6239-Fri-167/stops
-    url = 'https://getgo-api.herokuapp.com/' + '/trips/' + trip_id_first + '/stops'
+    # ASSUMPTION: any given trip for the same route variant always returns the same stops
+    # so we can just pick any trip (the first trip) from the route variant to obtain the stops
+    url = 'https://getgo-api.herokuapp.com/' + '/trips/' + trip['id'] + '/stops'
+    # https://getgo-api.herokuapp.com/trips/6179-Fri-21865/stops
     response = HTTParty.get(url)
     stopsHash = JSON.parse(response.body)
     stopsArray = stopsHash['stops']
 
-    puts "---------- Task 2b: Stops names ----------"
+    stopNames = []
+    puts "---------- Task 2b: Stops names under the specified route variant ----------"
     stopsArray.each do |stop|
-      puts stop['name']
+      stopNames << stop['name']
     end
 
+    return stopNames
+    # TODO: order the stop names in order of stop sequence (need stop_times table for this)
+  end
 
-    # TODO: do route variant for buses (current solution only for trains)
+  def getDirection (trip, toStop, fromStop)
 
-    # ----- Task 3. Get stop_times from toStop, fromStop, date, and time -----
-    toStop = "UN"
-    fromStop = "SR"
-    date = '20161202'
-    route_id = '258-MI'
-    direction_id = 999  # To be determined
-
-    # First, determine the direction based on toStop and fromStop
-    # Use tripsArray obtained from previous step i.e. "6239-Fri-167", "6239-Fri-166"... "6239-Fri-148"
-    # pick the first two trips from the array, since each one represents one direction
-    # tripsArray[0] = "6239-Fri-167" is from Union (stop_sequence = 1) to Milton (stop_sequence = 9)
-    # tripsArray[1] = "6239-Fri-166" is from Milton (stop_sequence = 1) to Union (stop_sequence = 9)
-
-    url = 'https://getgo-api.herokuapp.com/' + '/trips/' + tripsArray[0]['id']
+    url = 'https://getgo-api.herokuapp.com/' + '/trips/' + trip['id']
     # https://getgo-api.herokuapp.com/trips/6239-Fri-167/stop_times
     response = HTTParty.get(url)
     stopTimesHash = JSON.parse(response.body)
@@ -82,6 +95,15 @@ class WelcomeController < ApplicationController
     else
        direction_id = 1 - stopTimesHash['trip']['direction_id'].to_i # swapping 0 and 1
     end
+
+    return direction_id
+  end
+
+  def getArrivalTimes (date, route_id, route_variant, toStop, fromStop)
+
+    tripsArray = getTrips(date, route_id, route_variant)
+
+    direction_id = getDirection(tripsArray[0], toStop, fromStop)
 
     # get the stop_times from these trips
 
@@ -107,6 +129,7 @@ class WelcomeController < ApplicationController
       departureTimes << stopTimesArray.find { |st| st['stop_id'] == fromStop}['departure_time']
     end
 
+
     puts "---------- Task 3c: departure times ----------"
     puts departureTimes
     # ["08:45:00", "08:18:00", "08:06:00", "07:56:00", "07:45:00", "07:33:00", "07:20:00", "07:03:00", "06:42:00", "06:18:00"]
@@ -118,6 +141,6 @@ class WelcomeController < ApplicationController
     # TODO: take care of bus routes that replace trains in non-rush hours (e.g. Bus 21 for Milton Train)
     # TODO: test other train route
 
-    byebug;
   end
+
 end
